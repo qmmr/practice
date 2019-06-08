99c4e6d194181aa871a3f263f47127cf77d86629
Marcin Kumorek <kumeek@gmail.com>
2019-06-08 11:53:05 +0200

    Add two routes that return different html :+1:

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.use('/users', (req, res, next) => {
+  console.log('/users')
+  res.send('<h1>You are on /users page! 😉</h1>')
+})
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from express.js! 👋</h1>')
+})
+
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}...`)
 })
