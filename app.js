df13fe68cf9c61aa0a5739b357536bd0a2604947
Marcin Kumorek <kumeek@gmail.com>
2019-06-10 12:59:27 +0200

    Add static "public" folder, style with CSS :nail_care:

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
