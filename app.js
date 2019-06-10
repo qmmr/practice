02cefec6b4ed70486762a58fdbfe41a326ecbaf9
Marcin Kumorek <kumeek@gmail.com>
2019-06-10 15:43:01 +0200

    Add pug rendering engine and use it in index page

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -17,8 +20,9 @@ app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.get('/', (req, res, next) => {
-  console.log('rootDir', rootDir)
-  res.status(404).sendFile(path.join(__dirname, 'views', 'index.html'))
+  // console.log('rootDir', rootDir)
+  // res.sendFile(path.join(__dirname, 'views', 'index.html'))
+  res.render('index', { pageTitle: 'Home Page' })
 })
 
 // Generic 404 page
