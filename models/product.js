7ed6a853382c385d8d50c11fcf3e4d1c40ea9b64
Marcin Kumorek <kumeek@gmail.com>
2019-06-23 14:48:38 +0200

    Cleanup :speak_no_evil:

--- models/product.js
+++ models/product.js
@@ -1,15 +1,9 @@
-const fs = require('fs').promises
-const path = require('path')
 const { query } = require('../utils/db')
 
 const intersectionBy = require('lodash/intersectionBy')
-const uuidV4 = require('uuid/v4')
-
-const FILE_PATH = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
 
 module.exports = class Product {
   constructor({ title, description, imageUrl, price }) {
-    this.product_id = uuidV4()
     this.title = title
     this.description = description
     this.imageUrl = imageUrl
