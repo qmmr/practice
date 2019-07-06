0a586f0bc44130eed34a073244161ad0b28ff123
Marcin Kumorek <kumeek@gmail.com>
2019-07-06 14:28:22 +0200

    Add mongoose User model :muscle:

--- models/user.js
+++ models/user.js
@@ -1,88 +1,16 @@
-const { ObjectId } = require('mongodb')
+const mongoose = require('mongoose')
 
-const { getDB } = require('../utils/db')
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  cart: {
+    products: [
+      {
+        product_id: { type: mongoose.Types.ObjectId, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+})
 
-class User {
-  constructor({ _id, name, email, cart = { products: [] } }) {
-    this._id = _id
-    this.name = name
-    this.email = email
-    this.cart = cart
-  }
-
-  async addToCart(productId) {
-    const db = getDB()
-    const collection = db.collection('users')
-
-    // Check if productId is in the cart
-    const foundIndex = this.cart.products.findIndex(p => p._id.toString() === productId)
-
-    console.log('foundIndex: ', foundIndex)
-    if (foundIndex === -1) {
-      // Add new product
-      this.cart = {
-        products: [
-          ...this.cart.products,
-          {
-            _id: new ObjectId(productId),
-            quantity: 1,
-          },
-        ],
-      }
-    } else {
-      // Update quantity
-      this.cart.products[foundIndex].quantity = this.cart.products[foundIndex].quantity + 1
-    }
-
-    // console.log('productId: ', productId)
-    // console.log('this.cart: ', this.cart)
-    // console.log('this._id: ', this._id)
-
-    // Save the cart
-    const { modifiedCount } = await this.saveCart()
-
-    return modifiedCount
-  }
-
-  async getCart() {
-    const db = getDB()
-    const collection = db.collection('products')
-    // console.log('this.cart: ', this.cart)
-    const productIds = this.cart.products.map(p => new ObjectId(p._id))
-    // console.log('productIds: ', productIds)
-    const products = await collection.find({ _id: { $in: productIds } }).toArray()
-    products.forEach(product => {
-      // console.log('product without quantity: ', product)
-      product.quantity = this.cart.products.find(p => p._id.toString() === product._id.toString()).quantity
-      // console.log('product with quantity: ', product)
-
-      return product
-    })
-    // console.log('products: ', products)
-
-    return {
-      products,
-    }
-  }
-
-  async saveCart(cart = this.cart) {
-    const db = getDB()
-    const collection = db.collection('users')
-
-    // Save the cart
-    const result = await collection.updateOne({ _id: new ObjectId(this._id) }, { $set: { cart } })
-    // console.log('saveCart -> result: ', Object.keys(result))
-    // console.log('saveCart -> result.modifiedCount: ', result.modifiedCount)
-
-    return result
-  }
-
-  async removeFromCart(productId) {
-    // Find the product in the cart and remove it
-    this.cart.products = this.cart.products.filter(product => product._id.toString() !== productId)
-
-    return await this.saveCart()
-  }
-}
-
-module.exports = User
+module.exports = mongoose.model('User', userSchema)
