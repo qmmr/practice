42af254c63e32e6b1def5003aad19dbf56b37a64
Marcin Kumorek <kumeek@gmail.com>
2019-08-02 09:59:18 +0200

    Cleanup

--- models/product.js
+++ models/product.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema
-const productSchema = new Schema({
+const productSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
